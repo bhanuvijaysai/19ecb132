98bcaf1f9cb29f0126395a515bd993e275569e8d
bhanuvijaysai <101688861+bhanuvijaysai@users.noreply.github.com>
2022-04-13 09:41:19 +0530

    Add files via upload

---
+++ checkprime.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+main()
+{
+	int n,i=2,c=0;
+	printf("\n enter the value of n");
+	scanf("%d",&n);
+	while((i<n)&&(n>1))
+	{
+		if(n%i==0)
+		{
+		c=1;
+		break;	
+		}
+		i++;
+	}
+	if(c==0)
+	printf("%d is prime number",n);
+	else
+	printf("%d is not a prime number",n);
+}
