502733fb61a5a42a3724c3414f9ccda472afeb3f
bhanuvijaysai <101688861+bhanuvijaysai@users.noreply.github.com>
2022-05-18 08:11:59 +0530

    Create factorial.c

---
+++ factorial.c
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+int main()
+{
+  int i,f=1,num;
+  printf("Enter a number: ");
+  scanf("%d",&num);
+  if (num>=10)
+    printf("the number is too large");
+  if (num<0)
+    printf("the number is negative");
+  else
+  {
+  for(i=1;i<=num;i++)
+      f=f*i;
+     
+  printf("Factorial of %d is: %d",num,f);
+  }
+  return 0;
+}
