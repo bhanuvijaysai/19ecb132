b991922524fdb51d83b3786962c5ccac06788358
Bhanu <101688861+bhanuvijaysai@users.noreply.github.com>
2022-05-31 12:40:49 +0000

    output

--- aton.c
+++ aton.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-main()
+int main()
 {
 	int n,i=1;
 	printf("\n Enter the value of n");
@@ -10,3 +10,7 @@ main()
 		i++;
 	}
 }
+/*output
+Enter the value of n 5
+  1  2  3  4  5
+  */
